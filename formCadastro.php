2c81d4e29dee007bd13ceecf3233f4add7ed8310
gasperpb <achillespb@hotmail.com>
2020-12-31 14:19:38 -0300

    Form cadastro

---
+++ formCadastro.php
@@ -0,0 +1,7 @@
+<form action="cadastrar.php" method="post">
+Nome:<input type="text" name="nome" id="nome"/><br>
+Login:<input type="text" name="nome" id="login"/><br>
+<input type=sumit value="Cadastrar">
+
+
+</form>
