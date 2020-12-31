4662a602451d0a39d0b6b956bc8796524343d9d9
gasperpb <achillespb@hotmail.com>
2020-12-31 19:57:26 -0300

    Form editar

--- formCadastro.php
+++ formCadastro.php
@@ -1,7 +1,5 @@
 <form action="cadastrar.php" method="post">
-Nome:<input type="text" name="nome" id="nome"/><br>
-Login:<input type="text" name="nome" id="login"/><br>
-<input type=sumit value="Cadastrar">
-
-
+Nome: <input type="text" name="nome" id="nome"/><br>
+Login: <input type="text" name="login" id="login"/><br>
+<input type="submit" value="Cadastrar">
 </form>
